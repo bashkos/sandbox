8c411430c9db0f0f9ef0ebf27dd3d194cbcc6672
bashkos <bashkos@gmail.com>
2017-05-25 12:34:56 +0800

    Add EmojiSelect page

--- components/App/index.js
+++ components/App/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import GiphySelectPage from '~pages/GiphySelectPage'; // eslint-disable-line import/no-unresolved, import/extensions
+import EmojiSelectPage from '~pages/EmojiSelectPage'; // eslint-disable-line import/no-unresolved, import/extensions
 import styles from './styles.css';
 
 const App = () => (
@@ -13,8 +14,10 @@ const App = () => (
       </h1>
       <ul className={styles.menu}>
         <li className={styles.menuItem}><Link to="/giphy-select">GiphySelect</Link></li>
+        <li className={styles.menuItem}><Link to="/emoji-select">EmojiSelect</Link></li>
       </ul>
       <Route path="/giphy-select" component={GiphySelectPage} />
+      <Route path="/emoji-select" component={EmojiSelectPage} />
     </div>
   </Router>
 );
